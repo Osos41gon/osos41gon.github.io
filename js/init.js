e369360126c34054b9a21b5e685e58c54dee02f0
Osos41gon <osos41gon@gmail.com>
2023-04-09 12:41:04 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyATJKnr71LQ_bkemgfb_KLaRF1qSHxzeWU",
+    authDomain: "osos41gon-8dc0f.firebaseapp.com",
+    projectId: "osos41gon-8dc0f",
+    storageBucket: "osos41gon-8dc0f.appspot.com",
+    messagingSenderId: "970806182347",
+    appId: "1:970806182347:web:f5b0b15af6466f0191364e",
+    measurementId: "G-STP9ZS8XYW"
+});
